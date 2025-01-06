2b55837bcf997e3bd9d810522edd4bf068748f44
Kevin Dockx <kevin.dockx@gmail.com>
2025-01-06 14:12:15 +0100

    Fix for EF Core migrations breaking change in .NET 9: https://learn.microsoft.com/en-us/ef/core/what-is-new/ef-core-9.0/breaking-changes#exception-is-thrown-when-applying-migrations-if-there-are-pending-model-changes

--- section-05/start/TravelInspiration.API/Shared/Persistence/Migrations/20250106123954_InitialMigration.Designer.cs
+++ section-05/start/TravelInspiration.API/Shared/Persistence/Migrations/20250106123954_InitialMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TravelInspiration.API.Shared.Persistence;
 
@@ -11,9 +12,11 @@ using TravelInspiration.API.Shared.Persistence;
 namespace TravelInspiration.API.Shared.Persistence.Migrations
 {
     [DbContext(typeof(TravelInspirationDbContext))]
-    partial class TravelInspirationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250106123954_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -113,11 +116,6 @@ namespace TravelInspiration.API.Shared.Persistence.Migrations
                         .IsRequired()
                         .HasMaxLength(200)
                         .HasColumnType("nvarchar(200)");
-
-                    b.Property<bool?>("Suggested")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("bit")
-                        .HasDefaultValue(false);
 
                     b.HasKey("Id");
 
